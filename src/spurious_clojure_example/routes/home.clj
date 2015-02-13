9c553d61914da6ead21aca80f0b718e2b3d7e4f2
Integralist <mark.mcdx@gmail.com>
2015-02-13 09:27:47 +0000

    Update project

--- src/spurious_clojure_example/routes/home.clj
+++ src/spurious_clojure_example/routes/home.clj
@@ -1,9 +1,10 @@
 (ns spurious-clojure-example.routes.home
   (:use [amazonica.aws.s3])
   (:require [compojure.core :refer :all]
+            [environ.core :refer [env]]
             [spurious-clojure-example.views.layout :as layout]))
 
-(if (System/getenv "DEBUG")
+(if (env :debug)
   (do
     (require '[spurious-aws-sdk-helper.core :as core])
     (core/configure
@@ -11,17 +12,16 @@
       {:sqs "b"}
       {:ddb (slurp "./resources/config/schema.yaml")})))
 
-; (def cred (assoc credentials :endpoint "s3.spurious.localhost:49154"))
+(def bucket-path "news-archive/dev/election2014-council_title")
 
-; (def content (apply str (line-seq
-;               (clojure.java.io/reader
-;                 (:object-content
-;                   (get-object
-;                     cred
-;                     :bucket-name "shared"
-;                     :key "news-archive/dev/election2014-council_title"))))))
+(def content
+  (apply str (line-seq
+               (clojure.java.io/reader
+                 (:object-content
+                   (get-object cred :bucket-name "shared" :key bucket-path))))))
+
 (defn home []
-  (layout/common [:h1 "hai"]))
+  (layout/common [:h1 content]))
 
 (defroutes home-routes
   (GET "/" [] (home)))
